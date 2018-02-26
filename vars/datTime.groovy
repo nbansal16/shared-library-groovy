8c8cbe283b1c610cc01c95f619644ff3b094dd7a
nbansal16 <36689548+nbansal16@users.noreply.github.com>
2018-02-26 17:24:39 +0530

    Update datTime.groovy

--- vars/datTime.groovy
+++ vars/datTime.groovy
@@ -2,8 +2,9 @@
 import java.text.SimpleDateFormat
 
 def call(){
+    echo "time1"
     def date = new Date()
     def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
-    echo '${sdf.format(date)}'
-    echo 'time'
+    echo "${sdf.format(date)}"
+    echo "time"
 }
